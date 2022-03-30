0aaf6831cdafdf606a514a21e6a21fc14b4436fb
Frankie Roberto <frankie@frankieroberto.com>
2022-03-30 23:05:22 +0100

    Keep a log of ignored start pages (#259)
    This makes new service start pages easier to spot

--- tasks/discover-start-pages.rb
+++ tasks/discover-start-pages.rb
@@ -27,6 +27,7 @@ start = 0
 results = []
 service_domains = []
 
+ignored_start_pages = File.read("tasks/ignored_start_page_urls.txt").split("\n")
 
 file = URI.open("https://www.gov.uk/api/search.json?filter_format=transaction&count=#{count}&start=#{start}")
 
@@ -35,11 +36,11 @@ json = JSON.parse(file.read)
 
 results = json['results']
 
-start_pages = results.collect {|x| "https://www.gov.uk" + x['link'] }
+start_pages = results.collect {|x| "https://www.gov.uk" + x['link'] } - ignored_start_pages
 
 missing_start_pages = start_pages.select do |start_page|
 
-  match = existing_services.detect {|service| service['start-page'] == start_page }
+  match = existing_services.detect {|service| Array(service['start-page']).include?(start_page) }
 
   !match
 end
