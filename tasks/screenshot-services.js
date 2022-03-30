1a60d12280ffd7f2d09a8fc5252bc7810956fdc8
Frankie Roberto <frankie@frankieroberto.com>
2022-03-30 23:04:13 +0100

    Update and add service screenshots (#256)

--- tasks/screenshot-services.js
+++ tasks/screenshot-services.js
@@ -8,10 +8,59 @@ const screenshotsFolder = path.join(__dirname, '..', 'app', 'assets', 'images', 
 
 var screenshotsToTake = [];
 
+const servicesToSkip = [
+  "analyse-school-performance",
+  "applications-manage-authority",
+  "apply-for-plant-export-certificates-and-inspections",
+  "book-pupil-theory-test",
+  "book-your-driving-test",
+  "book-your-theory-test",
+  "check-mot-history",
+  "check-vehicle-compatibility-e10-petrol",
+  "check-vehicle-recalls",
+  "claim-chargepoint-grants",
+  "claim-tax-relief-for-expenses-of-employment",
+  "comply-chemical-regulations",
+  "company-car-tax",
+  "consular-appointments",
+  "create-a-uk-catch-certificate",
+  "debt-respite-breathing-space",
+  "drone-registration",
+  "find-energy-certificate",
+  "find-property-information",
+  "get-help-with-esfa-services",
+  "give-feedback-on-care",
+  "goods-vehicle-movement-service",
+  "help-to-save",
+  "join-childcare-register",
+  "local-land-charges",
+  "log-coronavirus-test-site-results",
+  "manage-fleet-vehicles",
+  "money-claims",
+  "money-laundering-supervision",
+  "mot-testing",
+  "online-crime-reporting",
+  "overseas-crisis",
+  "recruit-an-apprentice",
+  "register-for-the-soft-drinks-industry-levy",
+  "register-nursery-daycare",
+  "register-trailer",
+  "relief-at-source",
+  "report-nursery-daycare-changes",
+  "sign-mortgage-deed",
+  "sorn",
+  "support-t-levels",
+  "vehicle-operator-licensing",
+  "vehicle-tax",
+  "view-designs-journal",
+  "view-the-orphan-works-register"
+]
+
 fs.readdirSync(servicesFolder).forEach(function(filename) {
   if (filename != '_template.json') {
     var project = JSON.parse(fs.readFileSync(servicesFolder + '/' + filename).toString());
     if (project.liveservice && project.phase != 'retired') {
+    if (project.liveservice && project.phase != 'retired' && !servicesToSkip.includes(filename.replace('.json', ''))) {
       screenshotsToTake.push({url: project.liveservice, service: filename.replace('.json', '')})
     }
   }
